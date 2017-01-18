c42a4f77a87de161b9684f23feda39479048b51e
Kallstad, Øyvind <oyvind.kallstad@avinor.no>
2017-01-18 11:13:44 +0100

    changes and fixes

--- getDocuments.ps1
+++ getDocuments.ps1
@@ -7,10 +7,7 @@ else {
 
 Get-ChildItem -Path '.\DocumentsUnpacked' -Directory | ForEach-Object {
     
-    $pathName = $_.BaseName
-    ".\Documents\$($_.BaseName).zip"
-
-    Compress-Archive -Path $_ -DestinationPath ".\Documents\$($_.BaseName).zip" -Force
+    Compress-Archive -Path "$($_.FullName)\*" -DestinationPath "$($documentsFolder.FullName)\$($_.BaseName).zip" -Force
     Rename-Item -Path "$($documentsFolder.FullName)\$($_.BaseName).zip" -NewName "$($documentsFolder.FullName)\$($_.BaseName).docx" -Force
 
 }
