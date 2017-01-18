61e5cec00ef19b26cb684150c57e77d97bb0bbc2
Kallstad, Øyvind <oyvind.kallstad@avinor.no>
2017-01-18 10:49:39 +0100

    changes

---
+++ do.ps1
@@ -0,0 +1,13 @@
+Get-ChildItem -Path '.\Documents' -File | ForEach-Object {
+    $documentName = $_.BaseName
+    if (-not (Test-Path -Path ".\DocumentsUnpacked\$($documentName)")) {
+        $destination = New-Item -Path ".\DocumentsUnpacked\$($documentName)" -ItemType Directory -Force
+    }
+    else {
+        $destination = Get-Item -Path ".\DocumentsUnpacked\$($documentName)"
+        Remove-Item -Path "$($destination.FullName)\*" -Recurse -Force
+    }
+    $zipFile = Copy-Item -Path $_.FullName -Destination "$($destination.FullName)\$($documentName).zip" -Force -PassThru
+    Expand-Archive -Path $zipFile.FullName -DestinationPath $destination -Force
+    Remove-Item -Path $zipFile.FullName
+}
